decff3f0bad2f0f5b878b35327e9d8ca72d7d750
Deneb Pulsipher <pulsipherd@gmail.com>
2018-06-07 19:40:25 -0600

    start school_controller.rb

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,17 +1,23 @@
 class SchoolsController < ApplicationController
 
   # GET /schools
-
+def index
+end
   # GET /schools/1
-
+def show
+end
   # GET /schools/new
-
+def new
+end
   # GET /schools/edit
-
+def edit
+end
   # POST /schools
-
+def create
+end
   # DELETE /schools
-
+def destroy
+end
   private
 
     # Use callbacks to share common setup or constraints between actions.
